f1bbf9e8f23e7731bad38b9e47e9e87c92b29bba
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2022-10-19 14:31:26 +0700

    added inital html & css code to app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ import React from 'react'
 
 class App extends React.Component {
 	render() {
-		return
+		return (
+			<div class="background main-layout">
+      <div class="nav-pic">
+        <img src="images/japan2.png" alt="" />
+      </div>
+      <div class="">
+        <div class="color name-text name-box">
+          <h1>Jacob Bidmead</h1>
+        </div>
+        <div class="description link1">
+          <h3>./projects</h3>
+        </div>
+        <div class="description link2">
+          <h3>./photography</h3>
+        </div>
+        <div class="description link3">
+          <h3>./links</h3>
+        </div>
+      </div>
+    </div>
+		)
 	}
 }
 
