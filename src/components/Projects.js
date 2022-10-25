4fab274728df08559d47650f3d5eda39e0e126e6
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2022-10-25 14:06:45 +0100

    added photos to projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import '../styles/Projects.css'
 
 class Projects extends React.Component {
 	render() {
 		return (
 	<>
-	  <div>Hello World</div>
+			 <div className="img-container">
+			  <img src="./images/project-img/MK1.png" className="img-container"/>
+			 </div>
+
+			<div>
+			 <img src="./images/project-img/MK3.png" className="img-container"/>
+			</div>
+
+			<div><img src="./images/project-img/MK4.png" className="img-container"/></div>
+			<div><img src="./images/project-img/MK2.png"  className="img-container"/></div>
+			<div><img src="./images/project-img/abnb1.png" className="img-container"/></div>
+	    <div><img src="./images/project-img/abnb2.png" className="img-container"/></div>
+			<div><img src="./images/project-img/abnb3.png" className="img-container"/></div>
+			<div><img src="./images/project-img/abnb4.png" className="img-container"/></div>
+	    <div><img src="./images/project-img/abnb5.png" className="img-container"/></div>
+			<div><img src="./images/project-img/abnb6.png" className="img-container"/></div>
+			<div><img src="./images/project-img/google.png" className="img-container"/></div>
+
 
 	</>
 		)
