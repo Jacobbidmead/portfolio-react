f84f70afb92014019c2c656f8505032087f0b582
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2022-11-01 11:53:47 +0000

    added toggle to show home button only if not on the home page

--- src/components/Top.js
+++ src/components/Top.js
@@ -3,10 +3,21 @@ import App from '../App.js'
 import '../styles/Top.css'
 
 class Top extends React.Component {
+	state = {
+		showHomeButton: false
+	}
+
+	toggleShowHomeButton = () => {
+		this.setState({
+			showHomeButton: true
+		})
+	}
+
+
 	render() {
 		return (
 		<>
-		<div className="top-bar">
+		{this.state.showHomeButton ? <div className="top-bar">
      <div className="home-button" onClick={e => this.props.toggleShowHome(e)}>+</div>
 			<div className="top-right">
 			 <div>
@@ -14,7 +25,7 @@ class Top extends React.Component {
 				</h1>
 				</div>
 			</div>
-		</div>
+		</div> : null}
 		</>
 		)
 	}
