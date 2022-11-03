8efbb8c2e08a610ecc1312567d61690893798cb6
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2022-11-03 12:37:02 +0000

    added europe section to images

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -2,16 +2,18 @@ import React from 'react'
 import '../styles/Photos.css'
 import saImageData from '../imageData/saImageData'
 import asiaImageData from '../imageData/asiaImageData'
+import europeImageData from '../imageData/europeImageData'
 
 let saDescription = "South & Central America: September 2017 - March 2018. Countries exhibited: Peru, Bolivia, Colombia & Mexico "
 let asiaDescription = "Japan, Vietnam & South Korea: September - November 2019"
+let europeDescription = "Life in Europe: 2014 - 2018 - London, Oslo, Palermo, Crete, Barcelona"
 
 class Photo extends React.Component {
 
 state = {
-	largePhoto: "images/sa/train.png",
-	allImages: saImageData,
-	imgDescription: saDescription
+	largePhoto: "images/asia/umbrella.png",
+	allImages: asiaImageData,
+	imgDescription: asiaDescription
 }
 
 changePic = (el) => {
@@ -36,6 +38,14 @@ changeToSa = () => {
 	})
 }
 
+changeToEurope = () => {
+	this.setState({
+		largePhoto: "images/europe/chef.png",
+		allImages: europeImageData,
+		imgDescription: europeDescription
+	})
+}
+
 
 	render() {
 		return (
@@ -43,9 +53,7 @@ changeToSa = () => {
 		<div className="photo-nav">
 			 <span onClick={e => this.changeToAsia(e)} className="change-photos">Far East</span>
 			 <span onClick={e => this.changeToSa(e)} className="change-photos">South America</span>
-			 <span onClick={e => this.changeToSa(e)} className="change-photos">Europe</span>
-			 <span onClick={e => this.changeToSa(e)} className="change-photos">Oceania</span>
-			 <span onClick={e => this.changeToSa(e)} className="change-photos">London</span>
+			 <span onClick={e => this.changeToEurope(e)} className="change-photos">Europe</span>
 		</div>
 			<div className="container">
 				<div className="photo-container">
