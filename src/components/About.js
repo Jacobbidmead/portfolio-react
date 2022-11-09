4b1f7ba1a661a5ddab6dc58fd7fb188a7261a517
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2022-11-09 11:46:04 +0000

    changed content in about section

--- src/components/About.js
+++ src/components/About.js
@@ -9,23 +9,24 @@ class About extends React.Component {
 			  <div className="about">About.</div>
 				 <div className="about-text">
                <span className="about-paragraph">
-	 							 Hi, I'm a Web Developer, Designer and Photographer based in London.
+	 							 Hi. I'm a <b>Web Developer, Designer and Photographer</b> based in London.
 								 </span>
 
                 <p><span className="about-paragraph">
-	 							Ive recently completed Tortuga Coders 11 week fullstack coding bootcamp in Koh Phangan - Thailand, where I gained experience of JavaScript, HTML, CSS, Node.js & React, amoung other languages and frameworks </span></p>
+	 							Ive recently completed Tortuga Coders 11 week fullstack coding bootcamp in Koh Phangan - Thailand, where I gained experience of <b>JavaScript, HTML, CSS, Node.js & React</b>, amoung other languages, frameworks and libarys. </span></p>
 
                  <span className="about-paragraph">
-	 							 Im currently looking for a junior position as Web Developer, whilst working on a number of personal and freelance projects that can be viewed in the projects section of this site and on my GitHub repository.
-       	 				 </span>
+	 							 Im currently looking for a junior position as Web Developer, whilst working on a number of personal and freelance projects that can be viewed in the projects section of this site and on my GitHub repository.  </span>
+
+
 
 									<span className="about-paragraph">
-	 								Im currently using React to create my projects and am looking for a position where I can gain further knowledge of React and further my JavaScript skills.
-									</span>
+	 								<b>I build my projects using React</b>; and am looking for a position where I can gain advanced skills using the libary and further my JavaScript knowledge.  </span>
 								  <span className="about-paragraph">
-	 							 A passionate photographer, Ive documentend my travels in South
-								 America, the USA, Asia & Australia over the last 8 years.
+	 							     A passionate photographer, Ive documentend my travels in South
+								  America, the USA, Asia, Europe & Australia over the last 8 years.
 								 </span>
+
 
          </div>
 
