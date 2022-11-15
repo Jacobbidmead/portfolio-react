4874b2f8b24ee1dc31bbfe053e7ade1a160ad758
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2022-11-15 21:11:25 +0000

    added content & styled about page
    - added links from link component

--- src/components/About.js
+++ src/components/About.js
@@ -7,56 +7,82 @@ class About extends React.Component {
       <>
         <div className="main-display">
           <div className="about-box-left">
-            <div className="about-text">
+            <div className="about-text-left">
               <span className="about-paragraph">
-                Hi. I'm a <b>Web Developer, Designer and Photographer</b> based
-                in London.
+                Hi. I'm a{" "}
+                <b>
+                  Full Stack Web Developer with interest in Web/UI Design and
+                  Photography,
+                </b>{" "}
+                originally from Suffolk and now based in London.
               </span>
 
               <p>
                 <span className="about-paragraph">
-                  Ive recently completed Tortuga Coders 11 week fullstack coding
-                  bootcamp in Koh Phangan - Thailand, where I gained extensive
-                  experience of <b>JavaScript, HTML, CSS, Node.js & React</b>,
-                  amoung other languages, frameworks and libarys.{" "}
+                  Im currently looking for a junior position as{" "}
+                  <b>Full Stack or Front End Web Developer</b>, whilst working
+                  on a number of personal and freelance projects that can be
+                  viewed in the projects section of this site and on my{" "}
+                  <b>GitHub</b> repository.{" "}
                 </span>
               </p>
+              <p>
+                <span className="about-paragraph right">
+                  <b>I build my projects using React & Node.js</b>; Im
+                  constantly practicing, watching and reading to advance skills
+                  with the <b>React library & Vanilla JavaScript</b>{" "}
+                </span>
+              </p>
+            </div>
+            <div className="links">
+              <h1>Contact me.</h1>
+              <div className="name-reveal">
+                [<i className="fa-solid fa-envelope"></i>
+                <span className="email revealed-text">
+                  jacob.bidmead.fullstack@outlook.com
+                </span>{" "}
+                ]
+              </div>
+
+              <a
+                href="https://github.com/Jacobbidmead"
+                className="gitlink name-reveal"
+                target="_blank"
+                rel="noreferrer"
+              >
+                [<i className="fa-brands fa-github"></i>
+                <span className="revealed-text">github.com/Jacobbidmead</span>]
+              </a>
             </div>
           </div>
 
           <div className="about-box-right">
-            <span className="about-paragraph">
-              Im currently looking for a junior position as Web Developer,
-              whilst working on a number of personal and freelance projects that
-              can be viewed in the projects section of this site and on my
-              GitHub repository.{" "}
-            </span>
-            <span className="about-paragraph">
-              <b>I build my projects using React</b>; and am looking for a
-              position where I can gain advanced skills using the libary and
-              further my JavaScript knowledge.{" "}
-            </span>
-            <span className="about-paragraph">
-              A passionate photographer, Ive documentend my travels in South
-              America, the USA, Asia, Europe & Australia over the last 8 years.
-            </span>
-          </div>
-          <div className="links name-reveal">
-            [<i className="fa-solid fa-envelope"></i>
-            <span className="email revealed-text">
-              jacob.bidmead.fullstack@outlook.com
-            </span>{" "}
-            ]
-          </div>
+            <div className="about-text-right">
+              <p>
+                <span className="about-paragraph right">
+                  I completed Tortuga Coders 11 week fullstack coding bootcamp
+                  in Koh Phangan - Thailand in mid October 2022, during the
+                  course I gained extensive experience of{" "}
+                  <b>JavaScript, HTML, CSS, Node.js & React</b>, amoung other
+                  languages, frameworks and libarys.{" "}
+                </span>
+              </p>
 
-          <div
-            href="https://github.com/Jacobbidmead"
-            className="gitlink links name-reveal"
-            target="_blank"
-            rel="noreferrer"
-          >
-            [<i className="fa-brands fa-github"></i>
-            <span className="revealed-text">github.com/Jacobbidmead</span>]
+              <p>
+                <span className="about-paragraph ">
+                  <b>A passionate photographer,</b> Ive documentend my travels
+                  in <b>South America, the USA, Asia, Europe & Australia</b>{" "}
+                  over the last 8 years.
+                </span>
+              </p>
+              <p>
+                <span className="about-paragraph last-right">
+                  I have a creative background; graduating from{" "}
+                  <b>Wimbledon College of Art </b>in 2013, studying{" "}
+                  <b>Print & Time Based Media.</b>
+                </span>
+              </p>
+            </div>
           </div>
         </div>
       </>
