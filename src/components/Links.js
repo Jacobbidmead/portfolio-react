8c2379fd22f27d45aa9502fa3d2d55c1dab87180
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2022-11-15 13:20:12 +0000

    added animation to links

--- src/components/Links.js
+++ src/components/Links.js
@@ -5,25 +5,26 @@ class Links extends React.Component {
   render() {
     return (
       <>
-        <div className="link-container">
-          <div>
-            <h1 className="contact-me">Contact Me.</h1>
-          </div>
-          <div>
-            <span className="links">
-              <i className="fa-solid fa-envelope"></i>
-              <span className="email">jacob.bidmead.fullstack@outlook.com</span>
-              <i className="fa-brands fa-github"></i>
-              <a
-                href="https://github.com/Jacobbidmead"
-                className="gitlink"
-                target="_blank"
-                rel="noreferrer"
-              >
-                github.com/Jacobbidmead
-              </a>
-            </span>
-          </div>
+        <div>
+          <h1 className="contact-me">Contact Me.</h1>
+        </div>
+
+        <div className="links name-reveal">
+          [<i className="fa-solid fa-envelope"></i>
+          <span className="email revealed-text">
+            jacob.bidmead.fullstack@outlook.com
+          </span>{" "}
+          ]
+        </div>
+
+        <div
+          href="https://github.com/Jacobbidmead"
+          className="gitlink links name-reveal"
+          target="_blank"
+          rel="noreferrer"
+        >
+          [<i className="fa-brands fa-github"></i>
+          <span className="revealed-text">github.com/Jacobbidmead</span>]
         </div>
       </>
     );
