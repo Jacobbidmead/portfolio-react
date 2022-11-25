96cab47deed108bdfb35d2443d93e708284fa348
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2022-11-25 10:32:28 +0000

    added content to magiKards project

--- src/components/Projects/MagiKards.js
+++ src/components/Projects/MagiKards.js
@@ -7,7 +7,7 @@ class MagiKards extends React.Component {
   };
   listenScrollEvent = (e) => {
     const backgroundBreakpoints = [0, 2000, 3000];
-    const colorsList = ["black", "lime"];
+    const colorsList = ["black", "#E2DEE6"];
 
     backgroundBreakpoints.forEach((breakpoint, position) => {
       if (
@@ -54,19 +54,16 @@ class MagiKards extends React.Component {
             <div
               style={{ color: "white", fontSize: "30px", marginRight: "100px" }}
             >
-              Fully working fullstack MVC AirBnB style web app, this personal
-              project was part of Tortuga Coders coding bootcamp. Users are able
-              to create a profile and upload house data, which is populated onto
-              the main app page.{" "}
+              Magikards is a fantasy based battle card game created by myself
+              and class mates Joshua Peoples & Mohammed Hammuri during the final
+              two weeks of Tortuga Coders bootcamp.
               <p>
-                Users, while logged in, can also edit house data and leave
-                comments. User data is stored in MongoDB and accessed with
-                Express.js.
+                The concept was top create a game using React that would take
+                two classes of cards, each with unique attributes that would
+                affect one another, with the strongest removing health from the
+                opposition player.
               </p>
-              <p>
-                Tecnhologies used - HTML, CSS, Bootstrap, Express.js, Node.js,
-                MongoDB.
-              </p>
+              Tecnhologies used - React, JavaScript, HTML, CSS.
             </div>
           </div>
           <div
@@ -114,14 +111,43 @@ class MagiKards extends React.Component {
               </Tilt>
             </div>
             <div
-              style={{ color: "white", marginLeft: "40px", textAlign: "right" }}
+              style={{
+                marginLeft: "40px",
+                textAlign: "right",
+                width: "50%",
+                marginLeft: "40%",
+              }}
             >
-              <h1>some text here</h1>
+              The view above is the home screen; players are able to select
+              either Ice or Fire, then enter thier name into the input above.
+              This allows the start of the game. Only when both players have
+              selected their class and entered thier name can they enter the
+              'Battlefield'.
+              <p>
+                The image on the left shows a pop up located on the home screen
+                that explains the rules of the game. This button is located in
+                the top right of the home screen and disapears when clicking
+                'info'.
+              </p>
             </div>
 
-            <div style={{ color: "white", marginTop: "70px" }}>
-              <h1>some text here</h1>
+            <div
+              style={{ marginTop: "70px", width: "50%", marginTop: " 100px" }}
+            >
+              This view below shows the Battlefield, the hand of cards is
+              randomly generated to the player. Once the player runs out of
+              cards, seven more cards will be randomly selected from the deck.
+              the view to the right is showing the selected cards of the two
+              players, when a card is selected it will be displayed as the main
+              card in the center of the screen. There is also a pass option.
+              <p>
+                Once both players have selected a card, the resulting card
+                effects will take place and health will be subtracked from the
+                relevent players. This project is still in the development phase
+                that we contintue to work on as a group.
+              </p>
             </div>
+
             <div style={{ marginTop: "100px" }}>
               {" "}
               <Tilt tiltMaxAngleX={10} tiltMaxAngleY={10} perspective={1000}>
