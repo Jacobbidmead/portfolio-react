3b641e117d4fc522e60d60d12a96b8f4678a4811
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2022-12-05 14:09:36 +0000

    created spinning animation onclick for color change button

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,18 @@ const random = () => {
   return backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
 };
 
+const animateCircle = {
+  clicked: {
+    scale: [1, 1.3, 1.3, 1, 1],
+    rotate: [0, 0, 360, 360, 0],
+    transition: {
+      ease: "easeInOut",
+      times: [0, 0.2, 0.5, 0.8, 1],
+      duration: 1,
+    },
+  },
+};
+
 class Home extends React.Component {
   state = {
     changeColor: false,
@@ -15,46 +27,44 @@ class Home extends React.Component {
   };
 
   toggleColorChange = () => {
-    console.log("work");
     this.setState({
       changeColor: true,
       backgroundColor: random(),
     });
   };
+
   render() {
     return (
       <>
         <div
           className="home-container"
-          style={{ backgroundColor: this.state.backgroundColor }}
+          style={{
+            backgroundColor: this.state.backgroundColor,
+            transition: "background-color 0.5s ease",
+          }}
         >
           <div className="name">JACOB</div>
 
-          <Tilt
-            glareEnable={false}
-            tiltMaxAngleX={10}
-            tiltMaxAngleY={10}
-            perspective={1000}
+          <motion.div
+            onClick={(e) => this.toggleColorChange(e)}
+            whileTap={"clicked"}
+            variants={animateCircle}
+            animate={{
+              scale: [1, 1.3, 1.3, 1, 1],
+              rotate: [0, 0, 360, 360, 0],
+            }}
+            transition={{
+              ease: "easeInOut",
+              times: [0, 0.2, 0.5, 0.8, 1],
+              duration: 1,
+            }}
+            className="circle"
           >
-            <motion.div
-              onClick={(e) => this.toggleColorChange(e)}
-              animate={{
-                scale: [1, 1.3, 1.3, 1, 1],
-                rotate: [0, 0, 360, 360, 0],
-              }}
-              transition={{
-                duration: 2,
-                ease: "easeInOut",
-                times: [0, 0.2, 0.5, 0.8, 1],
-              }}
-              className="circle"
-            >
-              {" "}
-              <div style={{ paddingTop: "60px" }}>
-                <span> Click me</span>
-              </div>
-            </motion.div>
-          </Tilt>
+            {" "}
+            <div style={{ paddingTop: "60px" }}>
+              <span> Click me</span>
+            </div>
+          </motion.div>
         </div>
       </>
     );
